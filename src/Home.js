b4b13a4a36503432ea33bf8ca45ff5d16749cf03
farzana <farzanaka22@gmail.com>
2023-11-21 22:25:52 +0530

    updation3

--- src/Home.js
+++ src/Home.js
@@ -76,49 +76,49 @@ function Home() {
                                 <div className="col-lg-1">
 
                                     <label for="orderid" class="form-label "><b>ORDER ID</b></label> <br />
-                                    <input type="text" class="form-control" placeholder='1011' id="orderid" aria-describedby="orderid1"
+                                    <input type="text" name='orderId' class="form-control" placeholder='1011' id="orderid" aria-describedby="orderid1"
                                         value={input.orderId} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-1">
                                     <label for="itemno" class="form-label "><b>ITEM NO</b></label> <br />
-                                    <input type="text" class="form-control"placeholder='1111' id="itemno" aria-describedby="itemno1"
+                                    <input type="text" name='itemNo' class="form-control"placeholder='1111' id="itemno" aria-describedby="itemno1"
                                         value={input.itemNo} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-1">
                                     <label for="matid" class="form-label "><b>MATERIAL ID</b></label>
-                                    <input type="text" class="form-control"placeholder=' 2222' id="matid" aria-describedby="matid1"
+                                    <input type="text" name='materialId' class="form-control"placeholder=' 2222' id="matid" aria-describedby="matid1"
                                         value={input.materialId} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-3">
                                     <label for="matdesc" class="form-label "><b>MATERIAL DESC</b></label><br /><br />
-                                    <input type="text" class="form-control"placeholder='Description about your Material' id="matdesc" aria-describedby="matdesc1"
+                                    <input type="text" name='materialdescription' class="form-control"placeholder='Description about your Material' id="matdesc" aria-describedby="matdesc1"
                                         value={input.materialdescription} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-1">
                                     <label for="currqty" class="form-label "><b>CURRENT QTY</b></label>
-                                    <input type="number" class="form-control"placeholder=' 10' id="currqty" aria-describedby="currqty1"
+                                    <input type="number" name='currentqty' class="form-control"placeholder=' 10' id="currqty" aria-describedby="currqty1"
                                         value={input.currentqty} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-1">
                                     <label for="preqty" class="form-label "><b>PREVIOUS QTY</b></label>
-                                    <input type="number" class="form-control"placeholder=' 8' id="preqty" aria-describedby="preqty1"
+                                    <input type="number" name='previousqty' class="form-control"placeholder=' 8' id="preqty" aria-describedby="preqty1"
                                         value={input.previousqty} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-1">
                                     <label for="qtydiff" class="form-label "><b>QTY DIFF</b></label><br />
-                                    <input type="number" class="form-control"placeholder=' 2' id="qtydiff" aria-describedby="qtydiff1"
+                                    <input type="number" name='qtydifference' class="form-control"placeholder=' 2' id="qtydiff" aria-describedby="qtydiff1"
                                         value={input.qtydifference} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
                                 <div className="col-lg-2">
                                     <label for="ordercomm" class="form-label "><b>ORDER COMMENT</b></label><br /><br />
-                                    <input type="text" class="form-control" id="ordercomm" placeholder='Comments about your Order'  aria-describedby="ordercomm1"
+                                    <input type="text" name='ordercommand' class="form-control" id="ordercomm" placeholder='Comments about your Order'  aria-describedby="ordercomm1"
                                         value={input.ordercommand} onChange={event => handleFormChange(index, event)}
                                     />
                                 </div>
